76a4c35462ab1de7b653bb411a1df1379d0cd29f
Andrei <Sirsafari@hotmail.com>
2022-03-22 06:30:50 +0200

    InitialCommit

---
+++ Snowmire/MyAccount/Models/SqlDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MyAccount.Models
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext() : base("SqlConnection")
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
