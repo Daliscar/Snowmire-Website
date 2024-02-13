963c0169c0180f1ce328049e8f6f8f6ccd83d755
Andrei <Sirsafari@hotmail.com>
2024-02-13 09:50:15 +0200

    Update HomeController.cs

--- Snowmire/SnowmireMVC/Controllers/HomeController.cs
+++ Snowmire/SnowmireMVC/Controllers/HomeController.cs
@@ -43,8 +43,8 @@ namespace SnowmireMVC.Controllers
         //    {
         //        var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
         //        var message = new MailMessage();
-        //        message.To.Add(new MailAddress("sirsafari@hotmail.com"));  // replace with valid value 
-        //        message.From = new MailAddress("gabidrake@hotmail.com");  // replace with valid value
+        //        message.To.Add(new MailAddress("testmail@hotmail.com"));  // replace with valid value 
+        //        message.From = new MailAddress("testmail@hotmail.com");  // replace with valid value
         //        message.Subject = "This is a test email message sent by an application. ";
         //        message.Body = /*string.Format(body, model.FromName, model.FromEmail, model.Message);*/"This is a test email message sent by an application. "; 
         //        message.IsBodyHtml = true;
@@ -53,8 +53,8 @@ namespace SnowmireMVC.Controllers
         //        {
         //            var credential = new NetworkCredential
         //            {
-        //                UserName = "Sirsafari@hotmail.com",  // replace with valid value
-        //                Password = "Daliscar552200"  // replace with valid value
+        //                UserName = "Test",  // replace with valid value
+        //                Password = "Testpass"  // replace with valid value
         //            };
         //            smtp.Credentials = credential;
         //            smtp.Host = "smtp-mail.outlook.com";
